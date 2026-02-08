249372ebef3200f2035d87b02bd0798fafb09246
Leonid Turnaev <lmars@mail.ru>
2026-02-08 20:25:10 +0700

    Переделал LengthCalculator на to_m

--- src/skpelectrics/electric_line_length_calculator.rb
+++ src/skpelectrics/electric_line_length_calculator.rb
@@ -6,14 +6,13 @@ module Lvm444Dev
 
     class LengthCalculator < SketchupVisitor::EdgeVisitor
       attr_reader :length
-      @@INCH_SCALE = 0.0254
 
       def initialize
         @length = 0.0
       end
 
       def visit_edge(edge)
-        @length += edge.length * @@INCH_SCALE
+        @length += edge.length.to_m
       end
     end
 
